b6dbd5d6604405846b65f2234023b4cc40b5c8c3
Anzhela Boiko <boikoanzhela@gmail.com>
2021-09-14 23:20:17 +0300

    fix homePage

---
+++ src/Views/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TrendingMovieList from '../TrendingMovieList/TrendingMovieList';
+
+export default function HomePage() {
+  return (
+    <>
+      <TrendingMovieList />
+    </>
+  );
+}
