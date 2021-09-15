1338f4b7968e1a3f2adc26f440ca60143534b3cf
Anzhela Boiko <boikoanzhela@gmail.com>
2021-09-16 00:15:48 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import './App.css';
 import AppBar from './AppBar/AppBar';
 import HomePage from './Views/HomePage';
+import MovieDetailsPage from './Views/MovieDetailsPage';
 import MoviesPage from './Views/MoviesPage';
+import NotFoundView from './Views/NotFoundView';
 
 function App() {
   return (
     <>
       <AppBar />
-      <Route path="/" exact component={HomePage} />
-      <Route path="/movies" component={MoviesPage} />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/movies" exact component={MoviesPage} />
+        <Route path="/movies/:movieId" component={MovieDetailsPage} />
+        <Route component={NotFoundView} />
+      </Switch>
     </>
   );
 }
