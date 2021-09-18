28154077135283fc3aa73c678af3c12b05a3f0a8
Anzhela Boiko <boikoanzhela@gmail.com>
2021-09-19 00:08:09 +0300

    add toastr

--- src/TrendingMovieList/TrendingMovieList.jsx
+++ src/TrendingMovieList/TrendingMovieList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import MovieList from '../MovieList/MovieList';
 import { getTrendingMovies } from '../Services/MoviesApi';
+import { toast } from 'react-toastify';
 
 export default function TrendingMovieList() {
   const [movies, setMovies] = useState([]);
@@ -10,7 +11,7 @@ export default function TrendingMovieList() {
       .then(response => {
         setMovies(response.results);
       })
-      .catch(err => new Error());
+      .catch(err => toast.error('Something go wrong'));
   }, []);
 
   return (
