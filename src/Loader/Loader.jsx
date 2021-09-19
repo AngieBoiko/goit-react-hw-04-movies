3431cff36717f86349dc1158332bac75171a2ad2
Anzhela Boiko <boikoanzhela@gmail.com>
2021-09-19 15:38:22 +0300

    add styles for loader

--- src/Loader/Loader.jsx
+++ src/Loader/Loader.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Loader from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import styles from './styles.module.css';
 
 export default function Spinner() {
   return (
-    <div className="Overlay">
+    <div className={styles.overlay}>
       <Loader type="Circles" color="#00BFFF" height={80} width={80} />
     </div>
   );
